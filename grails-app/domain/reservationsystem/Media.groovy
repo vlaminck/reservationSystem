a1692f88888c94428664f19a4f69c253c509103e
Steve Vlaminck <steve@vlaminck.com>
2011-02-13 16:31:46 -0600

    Added boolean isAvailable to Media domain class

--- grails-app/domain/reservationsystem/Media.groovy
+++ grails-app/domain/reservationsystem/Media.groovy
@@ -6,12 +6,14 @@ class Media {
   String format
   String title
   String description
+  Boolean isAvailable = true
 
   static constraints = {
     type(nullable: false)
     format(nullable: false, blank: false)
     title(nullable: false, blank: false)
     description(nullable: true, blank: true)
+    isAvailable(nullable: false)
   }
 }
 
