e281ece840b7955bc0df77bfb8cf79cb40fe5cbb
Steve Vlaminck <steve@vlaminck.com>
2011-02-20 18:00:36 -0600

    Fix for searching with one filter bug.

--- grails-app/controllers/reservationsystem/SearchController.groovy
+++ grails-app/controllers/reservationsystem/SearchController.groovy
@@ -18,51 +18,67 @@ class SearchController {
 //    }
 //    println "-------------------"
 
-    def searchTerms = params.query?.split(" ")
-    println searchTerms
+    if (params.type && params.format && params.query) {
 
-    def types = []
-    params.type?.each {
-      types << MediaType."${it}"
-    }
+      def searchTerms = params.query?.split(" ")
 
-    def formats = []
-    params.format?.each {
-      formats << MediaFormat."${it}"
-    }
-
-    def filteredResults = Media.createCriteria().list {
-      'in'("type", types)
-      'in'("format", formats)
-    }
-
-    def searchResults = []
-    filteredResults.each { media ->
-      searchTerms.each { term ->
-        if (media.title?.toLowerCase()?.contains(term.toLowerCase()) || media.artist?.toLowerCase()?.contains(term.toLowerCase())) {
-          searchResults << media
+      def types = []
+      if (params.type.toString().startsWith('[') && params.type.toString().endsWith(']')) {
+        params.type.each {
+          types << MediaType."${it}"
         }
       }
+      else {
+        types << MediaType."${params.type}"
+      }
+
+      def formats = []
+      if (params.format.toString().startsWith('[') && params.format.toString().endsWith(']')) {
+        params.format.each {
+          formats << MediaFormat."${it}"
+        }
+      }
+      else {
+        formats << MediaFormat."${params.format}"
+      }
+
+      def filteredResults = Media.createCriteria().list {
+        'in'("type", types)
+        'in'("format", formats)
+      }
+
+      def searchResults = []
+      filteredResults.each { media ->
+        searchTerms.each { term ->
+          if (media.title?.toLowerCase()?.contains(term.toLowerCase()) || media.artist?.toLowerCase()?.contains(term.toLowerCase())) {
+            searchResults << media
+          }
+        }
+      }
+
+      searchResults = searchResults.unique {a, b -> a.title <=> b.title}
+
+      def printResults = []
+      def audioResults = []
+      def videoResults = []
+
+      searchResults.each { media ->
+        if (media.type == MediaType.PRINT) {
+          printResults << media
+        }
+        else if (media.type == MediaType.AUDIO) {
+          audioResults << media
+        }
+        else if (media.type == MediaType.VIDEO) {
+          videoResults << media
+        }
+      }
+
+      return [results: [print: printResults, audio: audioResults, video: videoResults]]
+    }
+    else {
+      redirect(uri: "/")
     }
 
-    searchResults = searchResults.unique {a, b -> a.title <=> b.title}
-
-    def printResults = []
-    def audioResults = []
-    def videoResults = []
-
-    searchResults.each { media ->
-      if (media.type == MediaType.PRINT) {
-        printResults << media
-      }
-      else if (media.type == MediaType.AUDIO) {
-        audioResults << media
-      }
-      else if (media.type == MediaType.VIDEO) {
-        videoResults << media
-      }
-    }
-
-    return [results: [print: printResults, audio: audioResults, video: videoResults]]
   }
 }
